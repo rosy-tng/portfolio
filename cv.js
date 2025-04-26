ff7bd1440c592eaa59316471071dca58ea204052
rosy <rosy.tanguy1@gmail.com>
2025-04-26 19:50:14 +0200

    Premier commit de mon portfolio

---
+++ cv.js
@@ -0,0 +1,23 @@
+//EFFET PAILLETE
+document.addEventListener("DOMContentLoaded", function () {
+  const glitterLayer = document.querySelector(".glitter-layer");
+  const numDots = 150; // Nombre de paillettes
+
+  for (let i = 0; i < numDots; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("glitter-dot");
+
+    // Position aléatoire
+    dot.style.top = Math.random() * 100 + "%";
+    dot.style.left = Math.random() * 100 + "%";
+
+    // Délai d'animation aléatoire
+    dot.style.animationDelay = (Math.random() * 4).toFixed(2) + "s";
+
+    dot.style.width = dot.style.height = (Math.random() * 2 + 1) + "px";
+
+
+    glitterLayer.appendChild(dot);
+  }
+});
+
